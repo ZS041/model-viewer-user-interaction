e2d7abfd02bb3ab3e52a63659c22cd1c2b0c7c75
Zak <Zaksmith04@gmail.com>
2022-12-13 14:54:46 -0500

    Lettucecat initial test, abcd key sequence

---
+++ script.js
@@ -0,0 +1,34 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
+
+//User must press 'a' 'b' 'c' 'd' in sequence to dismiss poster
+const modelViewer = document.querySelector('model-viewer');
+const keySequence = ['a', 'b', 'c', 'd'];
+let keyIndex = 0;
+document.addEventListener('keydown', (event) => {
+  if (event.key === keySequence[keyIndex]) {
+    keyIndex++;
+  } else {
+    keyIndex = 0;
+  }
+  if (keyIndex === keySequence.length) {
+    modelViewer.dismissPoster();
+  }
+}
+);
